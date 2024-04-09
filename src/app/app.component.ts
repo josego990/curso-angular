175435c5f8975a0c720fadba742b212227c566d5
josego990 <josego99@gmail.com>
2024-04-08 21:38:06 -0600

    adding-axios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'curso-angular';
   dataSource: any;
-  displayedColumns: string[] = ['id', 'type', 'label'];
+  displayedColumns: string[] = ['id_producto', 'nombre', 'descripcion'];
 
   submitForm() {
     console.log('BOTONAZO!');
+  }
+
+  getProducts() {
+    console.log('BOTONAZO GET PRODUCTS!');
+    const apiUrl = 'http://3.82.197.47:3030/get-product-by-id';
+    let datas = { id_producto: 66 };
+
+    axios
+      .post(apiUrl, datas)
+      .then((response: any) => {
+        // La consulta fue exitosa, puedes acceder a los datos en response.data
+        console.log('Datos recibidos:', response.data);
+        this.dataSource = response.data;
+      })
+      .catch((err: any) => {
+        // Ocurrió un error al realizar la consulta
+        console.error('Error al realizar la consulta:', err);
+      });
   }
 }
